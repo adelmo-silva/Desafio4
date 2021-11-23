5380481cb8ff0e62acfa63e3f25ced57105239b2
Adelmo Silva <adelmotocomsono@gmail.com>
2021-11-22 21:32:39 -0300

    Única versão

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿{
+    Console.Write("Informe um valor: ");
+    double valor = double.Parse(Console.ReadLine());
+    
+    while(valor < 100)
+    {
+        Console.WriteLine("O valor informado está incorreto! ");
+        Console.Write("Informe um valor novamente: ");
+        valor = double.Parse(Console.ReadLine());
+    }
+
+    Console.WriteLine("Sucesso!");
+}
---
+++ Readme.md
@@ -0,0 +1,5 @@
+Crie um algoritmo que não permite que o usuário informe um valor menor que 100.
+
+Se ele informar um valor menor, deve mostrar uma mensagem e solicitar novamente.
+
+Se ele informar maior, ele deve mostrar uma mensagem de sucesso.
